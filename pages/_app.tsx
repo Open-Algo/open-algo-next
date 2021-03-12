09aaa6572de11eed778f7e0455395b1f6b683452
Matt Ramotar <matt.ramotar@jhu.edu>
2021-03-12 16:17:18 -0500

    First commit

---
+++ pages/_app.tsx
@@ -0,0 +1,17 @@
+import React, {useEffect} from 'react'
+import '../styles/globals.css'
+
+import { AppProps} from 'next/app'
+
+function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    const jssStyles = document.querySelector('#jss-server-side')
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles)
+    }
+  }, [])
+
+  return <Component {...pageProps} />
+}
+
+export default MyApp
