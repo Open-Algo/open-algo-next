a2f4394eb499d9196842087f989f419f2a5e5619
Matt Ramotar <matt.ramotar@jhu.edu>
2021-03-14 16:21:03 -0400

    Add Learn page

---
+++ pages/learn.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Box, Typography } from '@material-ui/core';
+import styles from '../styles/learn.module.scss';
+
+export default function Learn() {
+  return (
+    <Box className={styles.root}>
+      <Typography>Learn</Typography>
+    </Box>
+  );
+}
