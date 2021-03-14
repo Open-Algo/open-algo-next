03d1bd984b41a013840f1811e5151c3402b88aad
Matt Ramotar <matt.ramotar@jhu.edu>
2021-03-14 16:21:13 -0400

    Add Paterns page

---
+++ pages/patterns.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Box, Typography } from '@material-ui/core';
+import styles from '../styles/patterns.module.scss';
+
+export default function Patterns() {
+  return (
+    <Box className={styles.root}>
+      <Typography>Patterns</Typography>
+    </Box>
+  );
+}
