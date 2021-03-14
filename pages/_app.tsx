95194483bfe413fd6faaa93c1ee303dca5e4bcc1
Matt Ramotar <matt.ramotar@jhu.edu>
2021-03-14 16:03:47 -0400

    Add App

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,20 @@
-import React, {useEffect} from 'react'
-import '../styles/globals.css'
-
-import { AppProps} from 'next/app'
+import React, { useEffect } from 'react';
+import { AppProps } from 'next/app';
+import Layout from '../src/components/Layout';
 
 function MyApp({ Component, pageProps }: AppProps) {
   useEffect(() => {
-    const jssStyles = document.querySelector('#jss-server-side')
+    const jssStyles = document.querySelector('#jss-server-side');
     if (jssStyles) {
-      jssStyles.parentElement.removeChild(jssStyles)
+      jssStyles.parentElement.removeChild(jssStyles);
     }
-  }, [])
+  }, []);
 
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
-export default MyApp
+export default MyApp;
