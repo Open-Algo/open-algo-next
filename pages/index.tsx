13fbcf27bb79055b1593fa57e31cf0b241567de0
Matt Ramotar <matt.ramotar@jhu.edu>
2021-03-14 16:05:22 -0400

    Add Home page

---
+++ pages/index.tsx
@@ -0,0 +1,24 @@
+import { GetServerSideProps } from 'next';
+import { getSession } from 'next-auth/client';
+import { Box, Typography } from '@material-ui/core';
+import styles from '../styles/Home.module.scss';
+
+export default function Home({ session }) {
+  return (
+    <Box className={styles.root}>
+      <Typography
+        variant="h4"
+        style={{ fontWeight: 'bold' }}
+      >{`Hey, ${session.user.name}!`}</Typography>
+    </Box>
+  );
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+  const session = await getSession({ req });
+  return {
+    props: {
+      session,
+    },
+  };
+};
