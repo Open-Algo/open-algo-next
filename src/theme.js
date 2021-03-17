7ba7e808b21da06176902637d583ee506f8328b1
Matt Ramotar <matt.ramotar@jhu.edu>
2021-03-17 16:53:04 -0400

    Clean

--- src/theme.js
+++ src/theme.js
@@ -1,7 +1,6 @@
 import { createMuiTheme } from '@material-ui/core/styles';
 import { red } from '@material-ui/core/colors';
 
-// Create a theme instance.
 const theme = createMuiTheme({
   palette: {
     type: 'dark',
@@ -25,31 +24,3 @@ const theme = createMuiTheme({
 });
 
 export default theme;
-
-// export interface Palette {
-//   common: CommonColors;
-//   type: PaletteType;
-//   contrastThreshold: number;
-//   tonalOffset: PaletteTonalOffset;
-//   primary: PaletteColor;
-//   secondary: PaletteColor;
-//   error: PaletteColor;
-//   warning: PaletteColor;
-//   info: PaletteColor;
-//   success: PaletteColor;
-//   grey: Color;
-//   text: TypeText;
-//   divider: TypeDivider;
-//   action: TypeAction;
-//   background: TypeBackground;
-//   getContrastText: (background: string) => string;
-//   augmentColor: {
-//     (
-//       color: ColorPartial,
-//       mainShade?: number | string,
-//       lightShade?: number | string,
-//       darkShade?: number | string
-//     ): PaletteColor;
-//     (color: PaletteColorOptions): PaletteColor;
-//   };
-// }
