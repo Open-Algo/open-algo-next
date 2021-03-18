2156bc14fe01fb04f2b1b0814d6662822f1ddccf
Matt Ramotar <matt.ramotar@jhu.edu>
2021-03-18 17:24:46 -0400

    Add helper function that gets data for radar chart

---
+++ src/helpers/getDataForRadarChart.ts
@@ -0,0 +1,31 @@
+import theme from '../theme';
+import { User } from '../../types';
+import MAP_OF_PROBLEMS from '../../constants/counts/problems';
+import PROBLEMS from '../../constants/labels/problems';
+
+export default function getDataForRadarChart({ user }: { user: User }) {
+  const dataForRadarChart = {
+    labels: PROBLEMS,
+    datasets: [
+      {
+        backgroundColor: theme.palette.primary.main,
+        borderColor: theme.palette.primary.main,
+        pointBackgroundColor: theme.palette.info.main,
+        pointBorderColor: theme.palette.info.main,
+        pointHoverBackgroundColor: theme.palette.secondary.light,
+        pointHoverBorderColor: theme.palette.info.light,
+        data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      },
+    ],
+  };
+
+  Object.entries(user.problemsByGroup)
+    .sort()
+    .forEach(([group, problems]: [string, { _total: number }], idx: number) => {
+      const numSolvedProblems = problems._total;
+      const numProblems = MAP_OF_PROBLEMS[group];
+      dataForRadarChart.datasets[0].data[idx] = numSolvedProblems / numProblems;
+    });
+
+  return dataForRadarChart;
+}
--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,2 +1,3 @@
 export { default as toTitleCase } from './toTitleCase';
 export { default as isProblemSolved } from './isProblemSolved';
+export { default as getDataForRadarChart } from './getDataForRadarChart';
