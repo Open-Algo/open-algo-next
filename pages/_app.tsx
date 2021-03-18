90f0b0a8ba58d2b90b483ecc5b5439d29606350a
Matt Ramotar <matt.ramotar@jhu.edu>
2021-03-18 10:05:46 -0400

    Use Apollo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,17 @@
 import React, { useEffect } from 'react';
+import Head from 'next/head';
 import { AppProps } from 'next/app';
 import { ThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import theme from '../src/theme';
+import { ApolloProvider } from '@apollo/client';
+
+import { useApollo } from '../lib/apollo';
 import Layout from '../src/components/Layout';
+import theme from '../src/theme';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const apolloClient = useApollo(pageProps.initialApolloState);
+
   useEffect(() => {
     const jssStyles = document.querySelector('#jss-server-side');
     if (jssStyles) {
@@ -14,12 +20,24 @@ function MyApp({ Component, pageProps }: AppProps) {
   }, []);
 
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </ThemeProvider>
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+        />
+      </Head>
+
+      <ApolloProvider client={apolloClient}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </ThemeProvider>
+      </ApolloProvider>
+    </>
   );
 }
 
