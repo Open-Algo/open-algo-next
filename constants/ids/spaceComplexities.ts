3f96ffe47eac2472e7381b9d6a13ee855b8e3ea9
Matt Ramotar <matt.ramotar@jhu.edu>
2021-03-19 11:49:40 -0400

    Add complexities

--- constants/ids/spaceComplexities.ts
+++ constants/ids/spaceComplexities.ts
@@ -1,3 +1,12 @@
 export default {
-  '604eae5f4a302cc4ce1ec318': 'O(N)',
+  '604eae5f4a302cc4ce1ec318': 'O(n)',
+  '604eae6b4a302cc4ce1ec319': 'O(1)',
+  '6054c2eaeb1e3febfc79cf22': 'O(log(n))',
+  '6054c4fb9ea7b1f69684a7f6': 'O(k log(n))',
+  '6054c4fb9ea7b1f69684a7f7': 'O(n log(n))',
+  '6054c4fc9ea7b1f69684a7f8': 'O(n^2)',
+  '6054c4fc9ea7b1f69684a7f9': 'O(mn)',
+  '6054c4fc9ea7b1f69684a7fa': 'O(2^n)',
+  '6054c4fd9ea7b1f69684a7fb': 'O(n!)',
+  '6054c4fd9ea7b1f69684a7fc': 'O(mn * min(m, n))',
 };
--- constants/ids/timeComplexities.ts
+++ constants/ids/timeComplexities.ts
@@ -1,3 +1,12 @@
 export default {
-  '604ea8b31ac4cfc2ae7f3d20': 'O(N)',
+  '604ea8b31ac4cfc2ae7f3d20': 'O(n)',
+  '604ea8cf1ac4cfc2ae7f3d22': 'O(n^2)',
+  '604ea8c21ac4cfc2ae7f3d21': 'O(1)',
+  '6054c65a6d1e16f739772ead': 'O(log(n))',
+  '6054c65b6d1e16f739772eae': 'O(k log(n))',
+  '6054c65b6d1e16f739772eaf': 'O(n log(n))',
+  '6054c65b6d1e16f739772eb0': 'O(mn)',
+  '6054c65c6d1e16f739772eb1': 'O(2^n)',
+  '6054c65c6d1e16f739772eb2': 'O(n!)',
+  '6054c65c6d1e16f739772eb3': 'O(mn * min(m, n))',
 };
