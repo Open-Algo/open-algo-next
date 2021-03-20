5de771fe7303ab6d0658f67d9532aa2089c3330a
Matt Ramotar <matt.ramotar@jhu.edu>
2021-03-20 16:22:24 -0400

    Change to TypeScript

---
+++ types/index.ts
@@ -0,0 +1,10 @@
+export type { default as Account } from './Account';
+export type { default as Complexity } from './Complexity';
+export type { default as Group } from './Group';
+export type { default as Problem } from './Problem';
+export type { default as Solution } from './Solution';
+export type { default as Session } from './Session';
+export type { default as Tag } from './Tag';
+export type { default as Token } from './Token';
+export type { default as User } from './User';
+export type { default as Problems } from './Problems';
