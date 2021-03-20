8d0bb3954ecabee91e68655b4f344bdb84ceb45d
Matt Ramotar <matt.ramotar@jhu.edu>
2021-03-20 16:21:53 -0400

    Add Problems type

---
+++ types/Problems.ts
@@ -0,0 +1,17 @@
+import Problem from './Problem';
+
+export default interface Problems {
+  Array: Problem[];
+  'Binary Search Tree': Problem[];
+  'Binary Tree': Problem[];
+  Binary: Problem[];
+  'Dynamic Programming': Problem[];
+  Graph: Problem[];
+  Heap: Problem[];
+  Interval: Problem[];
+  'Linked List': Problem[];
+  Matrix: Problem[];
+  Stack: Problem[];
+  String: Problem[];
+  Trie: Problem[];
+}
