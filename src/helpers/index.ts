8d65bef59ae788d9d8fe9a2cc21b051feac2e665
Matt Ramotar <matt.ramotar@jhu.edu>
2021-03-20 16:23:12 -0400

    Change to TypeScript

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -4,3 +4,5 @@ export { default as getDataForRadarChart } from './getDataForRadarChart';
 export { default as hasExplanation } from './hasExplanation';
 export { default as hasSolution } from './hasSolution';
 export { default as hasTemplate } from './hasTemplate';
+export { default as getCountSolvedProblems } from './getCountSolvedProblems';
+export { default as getCountProblems } from './getCountProblems';
