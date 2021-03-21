84568e732fb165ffc86f18dea46a198665206418
Matt Ramotar <matt.ramotar@jhu.edu>
2021-03-21 14:41:22 -0400

    Update constants

--- constants/labels/problems.ts
+++ constants/labels/problems.ts
@@ -1,10 +1,12 @@
 export default [
   'Array',
   'Binary Search Tree',
+  'Binary Search',
   'Binary Tree',
   'Binary',
   'Dynamic Programming',
   'Graph',
+  'Hash Table',
   'Heap',
   'Interval',
   'Linked List',
